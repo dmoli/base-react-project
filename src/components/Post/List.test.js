f29a977f10288528837e945aba4de3a9d33ffc64
Diego Molina <diego.jose.molina@live.com>
2017-07-27 22:36:50 -0300

    DOM test in Post Item added

--- src/components/Post/List.test.js
+++ src/components/Post/List.test.js
@@ -14,11 +14,20 @@ const items = [
     id: 2,
     title: 'title test 2',
   },
+  {
+    id: 3,
+    title: 'title test 3',
+  },
 ];
 
 describe('Post <List />', () => {
   it('rendered correctly', () => {
     const component = createComponentWithIntl(<List posts={items} />);
     expect(component).toMatchSnapshot();
+  });
+
+  it('should render 3 <Item /> components', () => {
+    const component = shallow(<List posts={items} />);
+    expect(component.find(Item).length).toBe(3);
   });
 });
